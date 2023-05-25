43849e3ee58e18e3c658839b16b33dc33f735a12
mukhnaya <mosesmukhanya@gmail.com>
2023-05-25 08:47:21 +0000

    task 2

--- 0x00-challenge/2-sort.rb
+++ 0x00-challenge/2-sort.rb
@@ -17,12 +17,12 @@ ARGV.each do |arg|
     i = 0
     l = result.size
     while !is_inserted && i < l do
-        if result[i] < i_arg
-            i += 1
+        if result[i] > i_arg
+          result.insert(i - 1, i_arg)
+          is_inserted = true
+          break
         else
-            result.insert(i - 1, i_arg)
-            is_inserted = true
-            break
+          i += 1
         end
     end
     result << i_arg if !is_inserted
